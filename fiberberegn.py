a0857e79724c248a6fde8de716846c7f81754ef8
lkaae2018 <40848476+lkaae2018@users.noreply.github.com>
2019-05-20 12:09:55 +0200

    Python file

---
+++ fiberberegn.py
@@ -0,0 +1,7 @@
+import math
+L=35
+#math.ceil runder op til det højste tal d.v.s 2,2 bliver rundet op til 3
+print(math.ceil(L/10))
+#Printer ud dæmpningen som skal lægges til fiberbugdettet afhængig af antal 10km fiberstrækninger
+print(math.ceil(L/10)*0.5)
+
