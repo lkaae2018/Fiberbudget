7cc139e94c8d1769587fa53960c7b3516e1dd052
lkaae2018 <lak@djhhadsten.dk>
2020-05-02 16:32:10 +0200

    ver2

--- fiberberegn.py
+++ fiberberegn.py
@@ -5,3 +5,15 @@ print(math.ceil(L/10))
 #Printer ud dæmpningen som skal lægges til fiberbugdettet afhængig af antal 10km fiberstrækninger
 print(math.ceil(L/10)*0.5)
 
+fiber=float(input("Indtast antal km SM fiber?"))
+splidsninger=int(input("Antal splidsninger?"))
+konnekteringer=int(input("Antal konnekteringer?"))
+#Udregn dæmpningen for 1310nm og 1550nm!
+#Træk sikerhedsmargin og reperationer dæmpning fra
+#Beregn en samlet dæmpning for de øvereste tal i variable!
+while True:
+    Transmitpower=float(input("Indtast TX power for SFP modul[dBm]"))
+    receivesensitivity=float(input("Indtast modtager følsomhed[dBm]"))
+    
+
+
